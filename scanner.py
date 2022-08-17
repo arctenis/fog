f192e489b364b756d4d346cb26318ae35e6f40b0
arctenis <arctenischloria@gmail.com>
2022-08-17 23:13:04 +0200

    FEAT scan token

--- scanner.py
+++ scanner.py
@@ -1,4 +1,4 @@
-import token_type
+from token_type import T
 from token import Token
 
 class Scanner:
@@ -8,14 +8,44 @@ class Scanner:
         self.start = 0
         self.current = 0
         self.line = 1
-        self.at_end = False
 
     def scan_tokens(self):
-        while not self.at_end:
+        while not self.at_end():
             self.start = self.current
             self.scan_token()
 
         self.tokens.append(Token("EOF", "", None, self.line))
 
     def scan_token(self):
-        pass
+        c = self.advance()
+        match c:
+            case "(": 
+                self.add_token(T.LEFT_PAREN)
+            case ")": 
+                self.add_token(T.RIGHT_PAREN)
+            case "{": 
+                self.add_token(T.LEFT_BRACE)
+            case "}": 
+                self.add_token(T.RIGHT_BRACE)
+            case ",": 
+                self.add_token(T.COMMA)
+            case ".": 
+                self.add_token(T.DOT)
+            case "-": 
+                self.add_token(T.MINUS)
+            case "+": 
+                self.add_token(T.PLUS)
+            case ";": 
+                self.add_token(T.SEMICOLON)
+            case "*": 
+                self.add_token(T.STAR)
+
+    def at_end(self):
+        return self.current >= len(self.source)
+
+    def advance(self):
+        return self.source[self.current+1]
+
+    def add_token(self, token_type, literal=None):
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(token_type, text, literal, self.line))
--- token_type.py
+++ token_type.py
@@ -1,47 +1,48 @@
-token_type = [
+from enum import Enum
+
+class T(Enum):
         # Single character tokens.
-        "LEFT_PAREN",
-        "RIGHT_PAREN",
-        "LEFT_BRACE",
-        "RIGHT_BRACE",
-        "COMMA",
-        "DOT",
-        "MINUS",
-        "PLUS",
-        "SEMICOLON",
-        "SLASH",
-        "STAR",
+        LEFT_PAREN = 1
+        RIGHT_PAREN = 2
+        LEFT_BRACE = 3
+        RIGHT_BRACE =4
+        COMMA = 5
+        DOT = 6
+        MINUS = 7
+        PLUS = 8
+        SEMICOLON = 9
+        SLASH = 10
+        STAR = 11
         
         # One or thow character tokens.
-        "BANG",
-        "BANG_EQUAL",
-        "EQUAL",
-        "EQUAL_EQUAL",
-        "GREATER",
-        "GREATER_EQUAL",
-        "LESS",
-        "LESS_EQUAL",
+        BANG = 12
+        BANG_EQUAL = 13
+        EQUAL = 14
+        EQUAL_EQUAL = 15
+        GREATER = 16
+        GREATER_EQUAL = 17
+        LESS = 18
+        LESS_EQUAL = 19
 
         # Literals.
-        "IDENTIFIER",
-        "STRING",
-        "NUMBER",
+        IDENTIFIER = 20
+        STRING = 21
+        NUMBER = 22
 
         # Keywords.
-        "AND",
-        "CLASS",
-        "ELSE",
-        "FALSE",
-        "FUN",
-        "FOR",
-        "IF",
-        "NIL",
-        "OR",
-        "PRINT",
-        "RETURN",
-        "SUPER",
-        "THIS",
-        "TRUE",
-        "VAR",
-        "WHILE",
-        ]
+        AND = 23
+        CLASS = 24
+        ELSE = 25
+        FALSE = 26
+        FUN = 27
+        FOR = 28
+        IF = 29
+        NIL = 30
+        OR = 31
+        PRINT = 32
+        RETURN = 33
+        SUPER = 34
+        THIS = 35
+        TRUE = 36
+        VAR = 37
+        WHILE = 38
