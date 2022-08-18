e2b2be15aaf5c31378db8956deee2bc997066bcc
arctenis <arctenischloria@gmail.com>
2022-08-18 12:15:04 +0200

    FEAT reserved words

--- scanner.py
+++ scanner.py
@@ -1,6 +1,7 @@
 from token_type import T
 from token import Token
 from fog import Fog
+from fog import Fog
 
 
 class Scanner:
@@ -59,8 +60,15 @@ class Scanner:
                 pass
             case "\n":
                 self.line += 1
+            case '"':
+                self.string()
             case _:
-                Fog.error(self.line, "Unexpected character.")
+                if c.isnumeric():
+                    self.number()
+                elif is_alpha(c):
+                    self.identifier()
+                else:
+                    Fog.error(self.line, "Unexpected character.")
 
     def add_token(self, token_type, literal=None):
         text = self.source[self.start : self.current]
@@ -84,3 +92,45 @@ class Scanner:
 
     def advance(self):
         return self.source[self.current + 1]
+
+    def string(self):
+        while self.peek() != '"' and not self.at_end():
+            if self.peek() == "\n":
+                self.line += 1
+            self.advance()
+
+        if self.at_end():
+            Fog.error(self.line, "Unterminated string.")
+            return None
+        self.advance()
+        value = self.source[self.start + 1 : self.current - 1]
+        self.add_token(T.STRING, value)
+
+    def number(self):
+        while self.peek().isdigit():
+            self.advance()
+
+        if self.peek() == "." and self.peek_next().isdigit():
+            self.advance()
+            while self.peek().isdigit():
+                self.advance()
+        self.add_token(T.NUMBER, float(self.source[self.start : self.current]))
+
+    def peek_next(self):
+        if self.current + 1 >= len(self.source):
+            return "\0"
+        return self.source[self.current + 1]
+
+    def is_alpha(self, c):
+        return (c >= "a" and c <= "z") or (c >= "A" and c <= "Z") or c == "_"
+
+    def identifier(self):
+        while self.peek().isalnum():
+            self.advance()
+        self.add_token(T.IDENTIFIER)
+
+    """
+    TODO
+    Remplacer isdigit() par une fonction maison is_digit()
+    Remplacer isalnum() par une fonction maison is_alphanumeric()
+    """
